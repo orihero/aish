c0068d92b5e6c17617a16d6b3e73f8a49a39187e
orihero <aka.orihero@gmail.com>
2025-09-14 13:11:56 +0500

    Remove ecosystem configuration files for backend, frontend, and admin applications; update backend port in ecosystem.config.cjs

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
       exec_mode: 'fork',
       env: {
         NODE_ENV: 'development',
-        PORT: 5000
+        PORT: 5001
       },
       env_production: {
         NODE_ENV: 'production',
