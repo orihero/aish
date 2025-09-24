b9c8b3d99e066a3cf78db7e28ae1773b0e3d8d7d
orihero <aka.orihero@gmail.com>
2025-09-24 23:28:10 +0500

    chore: update API URL in environment configuration
    - Changed the API URL from localhost to the production endpoint for deployment.
    - Commented out the previous localhost URL for clarity.

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,5 +1,6 @@
 // const url = "https://aish.yaqiin.app";
-const url = "http://localhost:5001";
+// const url = "http://localhost:5001";
+const url = "https://api.aish.uz";
 
 export const Env = {
     ApiUrl: `${url}/`,
